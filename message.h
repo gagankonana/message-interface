ceea6d796c444083b540df70b4b7b0e9b3d4c613
Gagan Nagaraj <gagandeep.konana@gmail.com>
2024-09-10 06:37:41 -0700

    Requirements

---
+++ message.h
@@ -0,0 +1,27 @@
+#ifndef _LIBRARY_MESSAGE_SYSTEM_MESSAGE_H
+#define _LIBRARY_MESSAGE_SYSTEM_MESSAGE_H
+
+/*
+Requirements:
+Messaging systems
+user to user message
+user to group message
+delete after certain period
+
+message - time, sender, 
+fetch messages, filter by sender, reciepient
+
+
+nouns and verbs
+Message  : content, time
+User
+Groups
+
+send message
+filter messages by sender, by reciepient
+
+
+
+*/
+
+#endif
