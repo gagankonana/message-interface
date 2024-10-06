e91d93d566e26f544af76fbea703180c88d1960c
Gagan Nagaraj <gagandeep.konana@gmail.com>
2024-10-06 16:31:33 -0700

    format code

--- src/consume.h
+++ src/consume.h
@@ -6,24 +6,25 @@
 
 class Consume {
 public:
-    Consume(const std::string& username) {
-        channel = AmqpClient::Channel::Create("localhost", 5672, "guest", "guest", "message-system", 131072);
-        channel->DeclareQueue(username, false, true, false, true);
-        channel->BasicConsume(username, "consumer-tag", true, false, false);
-    }
+  Consume(const std::string &username) {
+    channel = AmqpClient::Channel::Create("localhost", 5672, "guest", "guest",
+                                          "message-system", 131072);
+    channel->DeclareQueue(username, false, true, false, true);
+    channel->BasicConsume(username, "consumer-tag", true, false, false);
+  }
 
-    [[nodiscard]] std::string const read() {
-        AmqpClient::Envelope::ptr_t envelope;
-        if (channel->BasicConsumeMessage("consumer-tag", envelope, 0)){
-            std::string message_body = envelope->Message()->Body();
-            channel->BasicAck(envelope);
-            return message_body;
-        }
-        return "";  
+  [[nodiscard]] std::string const read() {
+    AmqpClient::Envelope::ptr_t envelope;
+    if (channel->BasicConsumeMessage("consumer-tag", envelope, 0)) {
+      std::string message_body = envelope->Message()->Body();
+      channel->BasicAck(envelope);
+      return message_body;
     }
+    return "";
+  }
 
 private:
-    AmqpClient::Channel::ptr_t channel; 
+  AmqpClient::Channel::ptr_t channel;
 };
 
 #endif
--- src/producer.h
+++ src/producer.h
@@ -6,27 +6,30 @@
 
 class Produce {
 public:
-    Produce(const std::string& _username) {
-        username = _username;
-        channel = AmqpClient::Channel::Create("localhost", 5672, "guest", "guest", "message-system", 131072);
-        channel->DeclareQueue(username, false, true, false, true);
-    }
+  Produce(const std::string &_username) {
+    username = _username;
+    channel = AmqpClient::Channel::Create("localhost", 5672, "guest", "guest",
+                                          "message-system", 131072);
+    channel->DeclareQueue(username, false, true, false, true);
+  }
 
-    void send(const std::string& _message) { 
-        auto message = AmqpClient::BasicMessage::Create(_message);
-        message->ContentType("application/json");
-        message->Type("XYZRequest");
-        message->AppId("a guid");
-        AmqpClient::Table headersTable;
-        headersTable.insert(std::pair<std::string, std::string>("Key-1", "value-1"));
-        headersTable.insert(std::pair<std::string, std::string>("Key-2", "value-2"));
-        message->HeaderTable(headersTable);
-        channel->BasicPublish("", username, message);
-    }
+  void send(const std::string &_message) {
+    auto message = AmqpClient::BasicMessage::Create(_message);
+    message->ContentType("application/json");
+    message->Type("XYZRequest");
+    message->AppId("a guid");
+    AmqpClient::Table headersTable;
+    headersTable.insert(
+        std::pair<std::string, std::string>("Key-1", "value-1"));
+    headersTable.insert(
+        std::pair<std::string, std::string>("Key-2", "value-2"));
+    message->HeaderTable(headersTable);
+    channel->BasicPublish("", username, message);
+  }
 
 private:
-    AmqpClient::Channel::ptr_t channel; 
-    std::string username;
+  AmqpClient::Channel::ptr_t channel;
+  std::string username;
 };
 
 #endif
